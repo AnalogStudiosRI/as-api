e2cc6fbddaddbfc501793a385f5b3e15ec47c049
Owen Buckley <obuckley@kenzanmedia.com>
2014-11-19 13:42:42 -0500

    AS-47 updating post build script

--- sh/post-build.sh
+++ sh/post-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "preparing deployment directory..."
-sudo rm -rvf /var/www/html/1000-bc.com/.htaccess
-sudo rm -rvf /var/www/html/1000-bc.com/*.phar
-sudo rm -rvf /var/www/html/1000-bc.com/*.php
+sudo rm -rvf /var/www/html/analogstudios.net/.htaccess
+sudo rm -rvf /var/www/html/analogstudios.net/*.phar
+sudo rm -rvf /var/www/html/analogstudios.net/*.php
 
 echo "deploying project..."
-sudo cp -v target/.htaccess /var/www/html/1000-bc.com/
-sudo cp -v target/*.phar /var/www/html/1000-bc.com/
-sudo cp -v target/config.php /var/www/html/1000-bc.com/
-sudo cp -v target/route.php /var/www/html/1000-bc.com/
+sudo cp -v target/.htaccess /var/www/html/analogstudios.net/
+sudo cp -v target/*.phar /var/www/html/analogstudios.net/
+sudo cp -v target/config.php /var/www/html/analogstudios.net/
+sudo cp -v target/route.php /var/www/html/analogstudios.net/
