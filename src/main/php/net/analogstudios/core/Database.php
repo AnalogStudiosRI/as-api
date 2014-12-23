430de67f24a6083b25f7275ca884e29be3b97042
Owen Buckley <owen@analogstudios.net>
2014-12-22 20:39:52 -0500

    AS-66 added update support to database, updateEvent to modal, and added tests

--- src/main/php/net/analogstudios/core/Database.php
+++ src/main/php/net/analogstudios/core/Database.php
@@ -23,11 +23,13 @@ class Database {
     "CREATED" => 201,
     "ERROR" => 500,
     "NOT_FOUND" => 404,
+    "NOT_MODIFIED" => 304,
     "SUCCESS" => 200
   );
   private static $STATUS_MESSAGE = array(
     200 => "Success",
     201 => "Created",
+    304 => "Not Modified",
     400 => "Bad Request",
     404 => "Not Found",
     500 => "Internal Service Error"
@@ -50,7 +52,7 @@ class Database {
   
   private function generateResponse ($code, $result = array(), $msg = ''){
     return array(
-      "status" => $code ? $code : 500,
+      "status" => $code ? $code : self::$STATUS_CODE["ERROR"],
       "message" => $msg ? $msg : self::$STATUS_MESSAGE[$code],
       "data" => $result
     );
@@ -115,7 +117,6 @@ class Database {
     };
 
     if($validParamsNeeded === count($queryParams) && $invalidParamError === ""){
-      $now = time();
       $query = rtrim($query, ", ");
       $keys = rtrim($keys, ", ");
       $values = trim($values, ", ");      
@@ -129,11 +130,10 @@ class Database {
         $result = array(
           "url" => "/api/" . $tableName . "/" . $db->lastInsertId(),
           "id" => $db->lastInsertId(),
-          "createdTime" => $now
+          "createdTime" => time()
         );
       }else{
         $code = self::$STATUS_CODE["ERROR"];
-        //var_dump($stmt->errorInfo());
         $invalidParamError = "Unknown Database error.";
       }
     }else{
@@ -142,68 +142,62 @@ class Database {
     
     return $this->generateResponse($code, $result, $invalidParamError);
   }
-//  
-//  public function update ($table, $params) {
-//    
-//        $db = $this->db;
-//    $response = array();
-//    $body = array();
-//    $status = 0;
-//
-//    if(preg_match(self::$ID_PATTERN, $eventId) && count($params) > 0) {
-//      $query = 'UPDATE events SET ';
-//      $queryParams = array();
-//
-//      foreach ($params as $key => $value) {
-//        if (in_array($key, self::$REQUIRED_CREATE_PARAMS)) {
-//          $query .= $key . "=:" . $key . ", ";
-//          $queryParams[':' . $key] = $value;
-//        }
-//      };
-//
-//      if(count($queryParams) > 0) {
-//        $query = rtrim($query, ", ");
-//        $query .= " WHERE id=:eventId";
-//        $queryParams[':eventId'] = $eventId;
-//
-//        $stmt = $db->prepare($query);
-//        $stmt->execute($queryParams);
-//
-//        if ($stmt->rowCount() === 1) {
-//          $status = 200;
-//          $body = array(
-//            "url" => '/api/events/' . $eventId,
-//            "id" => $eventId
-//          );
-//        } else if ($stmt->rowCount() === 0) {
-//          $stm = $db->prepare('SELECT * FROM events WHERE id=:id');
-//          $stm->bindValue(':id', $eventId, $db::PARAM_INT);
-//          $stm->execute();
-//
-//          $found = $stm->fetch($db::FETCH_NUM) > 0;
-//          
-//          $status = $found ? 304 : 404;
-//          $body["message"] = $found ? "Duplicate data, event not modified" : "Event Not Found";
-//        } else {
-//          $status = 500;
-//          $body["message"] = $stmt->errorInfo();
-//        }
-//      }else {
-//        $status = 400;
-//        $body["message"] = "Bad Request.  No valid params provided";
-//      }
-//    }else{
-//      $status = 400;
-//      $missing = !$eventId ? "id" : "params";
-//      $body["message"] = "Bad Request.  No " . $missing . " provided";
-//    }
-//
-//    //construct response
-//    $response["status"] = $status;
-//    $response["body"] = $body;
-//
-//    return $response;
-//  }
+  
+  public function update ($tableName, $id, $updateParams, $params) {
+    $db = $this->db;
+    $invalidParamError = '';
+    $result = array();
+
+    if(preg_match(self::$PATTERN["ID"], $id) && count($params) > 0) {
+      $query = 'UPDATE events SET ';
+      $queryParams = array();
+
+      foreach ($params as $key => $value) {
+        if (in_array($key, $updateParams)) {
+          $query .= $key . "=:" . $key . ", ";
+          $queryParams[':' . $key] = $value;
+        }
+      };
+
+      if(count($queryParams) > 0) {
+        $query = rtrim($query, ", ");
+        $query .= " WHERE id=:id";
+        $queryParams[":id"] = $id;
+        
+        $stmt = $db->prepare($query);
+        $stmt->execute($queryParams);
+
+        if ($stmt->rowCount() === 1) {
+          $code = self::$STATUS_CODE["SUCCESS"];
+          $result = array(
+            "url" => "/api/" . $tableName . "/" . $id,
+            "id" => $id
+          );
+        } else if ($stmt->rowCount() === 0) {
+          //echo "SELECT * FROM " . $tableName . " WHERE id=:id";
+          $stm = $db->prepare("SELECT * FROM " . $tableName . " WHERE id=:id");
+          $stm->bindValue(':id', $id, $db::PARAM_INT);
+          $stm->execute();
+
+          $found = $stm->fetch($db::FETCH_NUM) > 0;
+          $code = $found ? self::$STATUS_CODE["NOT_MODIFIED"] : self::$STATUS_CODE["NOT_FOUND"];
+          $invalidParamError = $found ? "Duplicate data, event not modified" : "Event Not Found";
+        } else {
+          $code = self::$STATUS_CODE["ERROR"];
+          $invalidParamError = "Unkown Database Error";
+        }
+      }else {
+        $code = self::$STATUS_CODE["BAD_REQUEST"];
+        $invalidParamError = "Bad Request.  No valid params provided";
+      }
+    }else{
+      $code = self::$STATUS_CODE["BAD_REQUEST"];
+      $missing = !$id ? "id" : "params";
+      $invalidParamError = "Bad Request.  No " . $missing . " provided";
+    }
+
+    return $this->generateResponse($code, $result, $invalidParamError);
+  }
 //  
 //  public function delete ($table, $ids) {
 //    $db = $this->db;
