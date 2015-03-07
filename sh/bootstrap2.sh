401aeecc894e56d76b7bf976c38690a18de42e37
Owen Buckley <owen@analogstudios.net>
2015-03-07 18:42:34 -0500

    TGH-27 got php5.5 and Apache 2.4 working together

--- sh/bootstrap2.sh
+++ sh/bootstrap2.sh
@@ -2,18 +2,24 @@
 
 echo -e "\n--- Starting installation... ---\n"
 
-echo -e "\n--- Updating repos / packages list ---\n"
+echo -e "\n--- Updating packages ---\n"
 apt-get update
-#apt-get install python-software-properties
 
-#add-apt-repository ppa:ondrej/php5-5.6
-#sudo apt-get update
-#sudo apt-get dist-upgrade1
+echo -e "\n--- Install base packages ---\n"
+apt-get -y install vim curl build-essential python-software-properties git > /dev/null 2>&1
+apt-get update
+
+echo -e "\n--- Add some custom repos to update our distro ---\n"
+add-apt-repository ppa:ondrej/php5 > /dev/null 2>&1
+add-apt-repository ppa:ondrej/apache2 > /dev/null 2>&1
+#add-apt-repository ppa:chris-lea/node.js > /dev/null 2>&1
+
+apt-get update
 
 echo -e "\n--- Installing PHP / Apache ---\n"
 apt-get install -y apache2 php5 libapache2-mod-php5
 
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant/target /var/www
+if ! [ -L /var/www/html ]; then
+  rm -rf /var/www/html
+  ln -fs /vagrant/target /var/www/html
 fi
