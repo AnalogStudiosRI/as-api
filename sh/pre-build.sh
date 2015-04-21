af2292c61a89a44aaa3d75c2ae0c7ce3e10b8444
Owen Buckley <owen@analogstudios.net>
2015-04-21 17:22:02 -0400

    testing build

--- sh/pre-build.sh
+++ sh/pre-build.sh
@@ -3,11 +3,11 @@
 echo "obtaining application dependencies..."
 composer install
 
-echo "*** Adding Composer Dependencies to the $PATH ***"
-export PATH=$PATH:src/main/php/vendor/bin
+echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE***"
+export PATH=$PATH:$WORKSPACE/src/main/php/vendor/bin
 
 echo "system info..."
 php --version
 composer --version
-phing -version
-phpunit -version
+#phing -version
+#phpunit -version
