98cf50dfcc9d8019a7bdc04f925fb12aa58e696f
Owen Buckley <owen@analogstudios.net>
2015-04-21 17:22:08 -0400

    testing build

--- sh/pre-build.sh
+++ sh/pre-build.sh
@@ -3,8 +3,8 @@
 echo "obtaining application dependencies..."
 composer install
 
-echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE***"
-export PATH=$PATH:$WORKSPACE/src/main/php/vendor/bin
+#echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE***"
+#export PATH=$PATH:$WORKSPACE/src/main/php/vendor/bin
 
 echo "system info..."
 php --version
