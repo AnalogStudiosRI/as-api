b5267c4a72bfb18a8d918f36580b41ddff2cfa83
Owen Buckley <owen@analogstudios.net>
2015-04-21 17:15:21 -0400

    updating build

--- sh/pre-build.sh
+++ sh/pre-build.sh
@@ -4,10 +4,7 @@ echo "obtaining application dependencies..."
 composer install
 
 echo "*** Adding Composer Dependencies to the $PATH ***"
-cat >> /home/jenkins/.bash_profile <<EOF
 export PATH=$PATH:src/main/php/vendor/bin
-EOF
-source /home/jenkins/.bash_profile
 
 echo "system info..."
 php --version
