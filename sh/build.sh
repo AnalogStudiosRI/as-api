d878b2d933e8485ad77609b87ca188e7278967aa
Owen Buckley <owen@analogstudios.net>
2015-05-12 23:10:04 -0400

    updating build script PATH

--- sh/build.sh
+++ sh/build.sh
@@ -4,7 +4,7 @@ echo "obtaining application dependencies..."
 composer install
 
 echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE ***"
-export PATH=$PATH:$WORKSPACE/src/main/php/vendor/bin
+export PATH=$PATH:$WORKSPACE/vendor/bin
 
 echo "system info..."
 php --version
