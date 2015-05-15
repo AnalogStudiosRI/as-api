7e1829fb5093d3e5cc0a598977e9bb9de2a77a84
Owen Buckley <owen@analogstudios.net>
2015-05-14 23:08:53 -0400

    AS-101 updated deploy script to copy form build directory

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -3,12 +3,12 @@
 # assumes build.sh has been run
 
 echo "preparing deployment directory..."
-sudo rm -rvf /var/www/analogstudios/.htaccess > /dev/null 2>&1
-sudo rm -rvf /var/www/analogstudios/*.phar > /dev/null 2>&1
-sudo rm -rvf /var/www/analogstudios/*.php > /dev/null 2>&1
+sudo rm -rvf /var/www/analogstudios/.htaccess
+sudo rm -rvf /var/www/analogstudios/*.phar
+sudo rm -rvf /var/www/analogstudios/*.php
 
 echo "deploying project..."
-sudo cp -v target/.htaccess /var/www/analogstudios/
-sudo cp -v target/*.phar /var/www/analogstudios/
-sudo cp -v target/config.php /var/www/analogstudios/
-sudo cp -v target/controller.php /var/www/analogstudios/
+sudo cp -v build/.htaccess /var/www/analogstudios/
+sudo cp -v build/*.phar /var/www/analogstudios/
+sudo cp -v build/config.php /var/www/analogstudios/
+sudo cp -v build/controller.php /var/www/analogstudios/
