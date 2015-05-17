ef1f45a6658cf17346ce462f6ca7eb5b7b96eb5e
Owen Buckley <owen@analogstudios.net>
2015-05-17 15:35:02 -0400

    AS-70 created class for wrapping mcrypt

---
+++ src/encrypt/EncryptDecrypt.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace encrypt;
+
+/**
+ *
+ * @author Owen Buckley
+ * @email owen@analogstudios.net
+ * @api as-api
+ * @package encrypt
+ * @class EncryptDecrypt
+ *
+ * @since 0.3.0
+ *
+ * @copyright 2015
+ *
+ */
+
+class EncryptDecrypt {
+  private $token = '';
+
+  function __construct($tokenPath = ''){
+    if($tokenPath !== '' && file_exists($tokenPath)){
+      $this->token = file_get_contents($tokenPath);
+    }else{
+      throw new \InvalidArgumentException('Invalid Constructor Params');
+    }
+  }
+
+  public function encryptData($data = ''){
+    if($data !== '') {
+      return base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($this->token), $data, MCRYPT_MODE_CBC, md5(md5($this->token))));
+    }else{
+      throw new \InvalidArgumentException('Invalid Method Param');
+    }
+  }
+
+  public function decryptData($data = ''){
+    if($data !== '') {
+      return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, md5($this->token), base64_decode($data), MCRYPT_MODE_CBC, md5(md5($this->token))), "\0");
+    }else{
+      throw new \InvalidArgumentException('Invalid Method Param');
+    }
+  }
+
+}
