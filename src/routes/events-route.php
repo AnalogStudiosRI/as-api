8ad435535b4a5fae38ab77da8aaceddedb332c2f
Owen Buckley <owen@analogstudios.net>
2015-05-18 20:41:50 -0400

    AS-60 commenting out routes that should have session auth

--- src/routes/events-route.php
+++ src/routes/events-route.php
@@ -19,28 +19,28 @@ $slim->get("/api/events/:id", function($eventId) use ($slim, $resource) {
   $slim->response->setBody(json_encode($response["data"]));
 });
 
-$slim->post("/api/events", function() use ($slim, $resource) {
-  $params = json_decode($slim->request->getBody(), true);
-  $response = $resource->createEvent($params);
-
-  $slim->response->status($response['status']);
-  $slim->response->setBody(json_encode($response["data"]));
-});
-
-$slim->put("/api/events/:id", function($eventId) use ($slim, $resource) {
-  $params = json_decode($slim->request->getBody(), true);
-  $response = $resource->updateEvent($eventId, $params);
-
-  $slim->response->status($response['status']);
-  $slim->response->setBody(json_encode($response["data"]));
-});
-
-$slim->delete("/api/events/:id", function($eventId) use ($slim, $resource) {
-  //$params = json_decode($slim->request->getBody(), true);
-  $response = $resource->deleteEvent($eventId);
-
-  $slim->response->status($response['status']);
-  $slim->response->setBody(json_encode($response["data"]));
-});
+//$slim->post("/api/events", function() use ($slim, $resource) {
+//  $params = json_decode($slim->request->getBody(), true);
+//  $response = $resource->createEvent($params);
+//
+//  $slim->response->status($response['status']);
+//  $slim->response->setBody(json_encode($response["data"]));
+//});
+//
+//$slim->put("/api/events/:id", function($eventId) use ($slim, $resource) {
+//  $params = json_decode($slim->request->getBody(), true);
+//  $response = $resource->updateEvent($eventId, $params);
+//
+//  $slim->response->status($response['status']);
+//  $slim->response->setBody(json_encode($response["data"]));
+//});
+//
+//$slim->delete("/api/events/:id", function($eventId) use ($slim, $resource) {
+//  //$params = json_decode($slim->request->getBody(), true);
+//  $response = $resource->deleteEvent($eventId);
+//
+//  $slim->response->status($response['status']);
+//  $slim->response->setBody(json_encode($response["data"]));
+//});
 
 ?>
