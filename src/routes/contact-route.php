876cd3f8071d6e9e969bc1bd3bf40a5708c720dd
Owen Buckley <obuckley@kenzan.com>
2015-08-22 22:00:52 -0400

    fixed syntax error

--- src/routes/contact-route.php
+++ src/routes/contact-route.php
@@ -10,7 +10,7 @@ $slim->post("/api/contact", function() use ($slim) {
   $response = services\ContactService::sendEmail($params["to"], $params["from"], $params["subject"], $params["message"]);
 
   $slim->response->status($response["status"]);
-  $slim->response->setBody(json_encode(array("message" => $response["message"])g));
+  $slim->response->setBody(json_encode(array("message" => $response["message"])));
 });
 
 ?>
