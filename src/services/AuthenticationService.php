1da8b6a99ee8a4078d0dbc3c06689f87a8148da8
Owen Buckley <owen@analogstudios.net>
2015-11-23 21:33:52 -0500

    AS-183 added JWT generation

--- src/services/AuthenticationService.php
+++ src/services/AuthenticationService.php
@@ -16,12 +16,61 @@ namespace services;
  *
  */
 
+use \Firebase\JWT\JWT;
+
 class AuthenticationService{
 
   private $db;
+  private $config;
 
-  public function __construct($dbConfig){
-    $this->db = new \PDO($dbConfig["dsn"], $dbConfig["username"], $dbConfig["password"]);
+  //XXX TODO should have config service injected
+  //XXX TODO should have JWT Service injected
+  public function __construct($config){
+    $this->config = $config;
+    $this->db = new \PDO($this->config["db"]["dsn"], $this->config["db"]["username"], $this->config["db"]["password"]);
+  }
+
+  private function generateJWT ($id, $username) {
+    $tokenId    = base64_encode(mcrypt_create_iv(32));
+    $issuedAt   = time();
+    $notBefore  = $issuedAt + 10;                     //Adding 10 seconds
+    $expire     = $notBefore + 60;                    // Adding 60 seconds
+    $serverName = $this->config["session"]["domain"];  // Retrieve the server name from config file
+
+    /*
+     * Create the token as an array
+     */
+    $jwtData = array(
+      "iat"  => $issuedAt,          // Issued at: time when the token was generated
+      "jti"  => $tokenId,           // Json Token Id: an unique identifier for the token
+      "iss"  => $serverName,        // Issuer
+      "nbf"  => $notBefore,         // Not before
+      "exp"  => $expire,            // Expire
+      "data" => [                   // Data related to the signer user
+        "userId"   => $id,          // userid from the users table
+        "userName" => $username,    // User name
+      ]
+    );
+
+    //XXX TODO THIS SHOULD NOT BE HARDCODED!!!!
+    $secretKey = base64_decode("jwtKey");
+
+    /*
+     * Encode the array to a JWT string.
+     * Second parameter is the key to encode the token.
+     *
+     * The output string can be validated at http://jwt.io/
+     */
+    $jwt = JWT::encode(
+      $jwtData,       //Data to be encoded in the JWT
+      $secretKey,     // The signing key
+      "HS512"         // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
+    );
+
+    $unencodedArray = ["jwt" => $jwt];
+    //echo json_encode($unencodedArray);
+
+    return $unencodedArray;
   }
 
   public function login ($username = null, $password = null) {
@@ -31,21 +80,21 @@ class AuthenticationService{
       $db = $this->db;
 
       $stmt = $db->prepare("SELECT * FROM users WHERE username=:username AND password=:password");
-      $stmt->bindValue(':username', $username);
-      $stmt->bindValue(':password', $password);
+      $stmt->bindValue(":username", $username);
+      $stmt->bindValue(":password", $password);
       $stmt->execute();
 
       $result = $stmt->fetchAll($db::FETCH_ASSOC);
-      var_dump($result);
 
       if(count($result) === 1){
         $response["success"] = true;
         $response["message"] = "Login Success";
+        $response["data"] = $this->generateJWT($result[0]["id"], $username);
       }else{
         $response["message"] = "Invalid Credentials";
       }
     }else{
-      throw new \InvalidArgumentException('Messing Credentials');
+      throw new \InvalidArgumentException("Messing Credentials");
     }
 
     return $response;
--- tests/services/AutheticationServiceTest.php
+++ tests/services/AutheticationServiceTest.php
@@ -8,23 +8,36 @@ use services as service;
 class AuthenticationServiceTest extends PHPUnit_Framework_TestCase{
   private static $username = "obuckley";
   private static $password = "testpwd";
-  private static $DB_CONFIG = array(
-    "dsn" => "mysql:host=127.0.0.1;dbname=asadmin_analogstudios_new_test",
-    "username" => "astester",
-    "password" => "t3st3r"
+  private static $CONFIG = array(
+    "session" => array(
+      "domain" => "analogstudios.thegreenhouse.io"
+    ),
+    "db" => array(
+      "dsn" => "mysql:host=127.0.0.1;dbname=asadmin_analogstudios_new_test",
+      "username" => "astester",
+      "password" => "t3st3r"
+    )
   );
 
   public function testLoginSuccess(){
-    $loginService = new service\AuthenticationService(self::$DB_CONFIG);
+    $loginService = new service\AuthenticationService(self::$CONFIG);
     $loginStatus = $loginService->login("astester", '$1$fDUPbgtB$Q3RER8dNV4aBbcw/dys8a/');
+
+    $this->assertArrayHasKey("success", $loginStatus);
+    $this->assertArrayHasKey("message", $loginStatus);
+    $this->assertArrayHasKey("data", $loginStatus);
+    $this->assertArrayHasKey("jwt", $loginStatus["data"]);
 
     $this->assertEquals($loginStatus["success"], true);
     $this->assertEquals($loginStatus["message"], "Login Success");
   }
 
   public function testLoginSuccessInvalidCredentials(){
-    $loginService = new service\AuthenticationService(self::$DB_CONFIG);
+    $loginService = new service\AuthenticationService(self::$CONFIG);
     $loginStatus = $loginService->login(self::$username, self::$password);
+
+    $this->assertArrayHasKey("success", $loginStatus);
+    $this->assertArrayHasKey("message", $loginStatus);
 
     $this->assertEquals($loginStatus["success"], false);
     $this->assertEquals($loginStatus["message"], "Invalid Credentials");
@@ -34,7 +47,7 @@ class AuthenticationServiceTest extends PHPUnit_Framework_TestCase{
    * @expectedException InvalidArgumentException
    */
   public function testLoginFailureNoCredentials(){
-    $loginService = new service\AuthenticationService(self::$DB_CONFIG);
+    $loginService = new service\AuthenticationService(self::$CONFIG);
     $loginService->login();
   }
 
@@ -42,7 +55,7 @@ class AuthenticationServiceTest extends PHPUnit_Framework_TestCase{
    * @expectedException InvalidArgumentException
    */
   public function testLoginFailureNoUsernameCredential(){
-    $loginService = new service\AuthenticationService(self::$DB_CONFIG);
+    $loginService = new service\AuthenticationService(self::$CONFIG);
     $loginService->login(null);
   }
 
@@ -50,7 +63,7 @@ class AuthenticationServiceTest extends PHPUnit_Framework_TestCase{
    * @expectedException InvalidArgumentException
    */
   public function testLoginFailureNoPasswordCredential(){
-    $loginService = new service\AuthenticationService(self::$DB_CONFIG);
+    $loginService = new service\AuthenticationService(self::$CONFIG);
     $loginService->login(self::$username, null);
   }
 }
