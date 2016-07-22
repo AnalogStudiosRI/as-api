5a13d27840d7510f5ad80f8a8e2c77d46cd7b673
Owen Buckley <owen@analogstudios.net>
2016-07-21 21:15:16 -0400

    update build script

--- bin/build.sh
+++ bin/build.sh
@@ -11,8 +11,8 @@ echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE ***"
 export PATH=$PATH:$WORKSPACE/vendor/bin
 
 echo "dependency info..."
-./vender/bin/phing -version
-./vendor/bin/phpunit --version
+phing -version
+phpunit --version
 
 #echo "building..."
 #./vender/bin/phing build
