ee19801f552ff694b7815f9e960750f2b47b0da1
Owen Buckley <owen@analogstudios.net>
2016-07-21 22:23:51 -0400

    local build working

--- bin/vagrant.sh
+++ bin/vagrant.sh
@@ -102,5 +102,5 @@ phpunit --version
 echo "*** Setting Up Env Config ***"
 cp /vagrant/ini/config-local.ini /var/www/config-env.ini
 
-apt-get -y install php-xdebug php7.1-xsl -y
+apt-get -y install php-xdebug php7.1-xsl php7.0-xml -y
 apt-get -y install sendmail
