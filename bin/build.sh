cd739e86c1acab26394826ee03f185038f8f73ff
Owen Buckley <owen@analogstudios.net>
2016-07-21 21:14:37 -0400

    update build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,19 +4,15 @@ echo "system info..."
 php --version
 composer -V
 
-# TODO this should come from projects composer install
-phing -version
-phpunit --version
-
 echo "installing application dependencies..."
 composer install
 
-#echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE ***"
-#export PATH=$PATH:$WORKSPACE/vendor/bin
+echo "*** Adding Composer Dependencies to the $PATH from $WORKSPACE ***"
+export PATH=$PATH:$WORKSPACE/vendor/bin
 
 echo "dependency info..."
-#./vender/bin/phing -version
-#./vendor/bin/phpunit --version
+./vender/bin/phing -version
+./vendor/bin/phpunit --version
 
 #echo "building..."
 #./vender/bin/phing build
