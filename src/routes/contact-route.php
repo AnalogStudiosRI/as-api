6bcc11b9628358db2c4b792d4547f9496157dc95
Owen Buckley <owen@analogstudios.net>
2016-11-06 13:08:33 -0500

    added honeypot check to contact route

--- src/routes/contact-route.php
+++ src/routes/contact-route.php
@@ -15,8 +15,15 @@ $slim->post("/api/contact", function() use ($slim, $envConfig) {
     "username" => $envConfig["mail.username"],
     "port" => $envConfig["mail.port"]
   ));
-  //TODO add spam protection - honeypot?
-  $response = $contactService->sendEmail($envConfig["mail.to"], $envConfig["mail.from"], $params["subject"], $params["message"]);
+
+  if(isset($params["username"])){
+    $response = array(
+      "status" => 200,
+      "message" => "Ah ah ah, you set the honeypot"
+    );
+  }else{
+    $response = $contactService->sendEmail($envConfig["mail.to"], $envConfig["mail.from"], $params["subject"], $params["message"]);
+  }
 
   $slim->response->status($response["status"]);
   $slim->response->setBody(json_encode(array("message" => $response["message"])));
